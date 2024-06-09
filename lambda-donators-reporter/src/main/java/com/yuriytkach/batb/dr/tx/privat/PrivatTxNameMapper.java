536f90858f516770e82abb7ee7570a7550f4024a
Yuriy Tkach <yuriytkach@gmail.com>
2024-06-09 17:36:50 +0300

    🧹 refactor(api): optimize donator name resolution in PrivatTxNameMapper
    This commit fine-tunes the name extraction algorithm in PrivatTxNameMapper to better handle variations in the name structure. The changes include:
    - Modified regular expressions to capture additional name patterns.
    - Adjusted the logic for selecting the appropriate name group from the match.
    - Changed the data structure for storing patterns from Set to List to maintain the order of patterns.
    
    These improvements should make the donator name resolution more robust and reliable. 🚀

--- lambda-donators-reporter/src/main/java/com/yuriytkach/batb/dr/tx/privat/PrivatTxNameMapper.java
+++ lambda-donators-reporter/src/main/java/com/yuriytkach/batb/dr/tx/privat/PrivatTxNameMapper.java
@@ -3,9 +3,9 @@ package com.yuriytkach.batb.dr.tx.privat;
 import static java.util.function.Predicate.not;
 import static java.util.regex.Pattern.compile;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
@@ -38,17 +38,25 @@ class PrivatTxNameMapper {
     default Optional<String> resolveNameFromPattern(final Pattern pattern, final String value) {
       final var matcher = pattern.matcher(value);
       if (matcher.matches()) {
+        final var namedGroups = matcher.namedGroups();
+        final var fnameGroup = namedGroups.containsKey("fname1") && matcher.group("fname1") != null
+                               ? matcher.group("fname1") : matcher.group("fname");
+        final var snameGroup = namedGroups.containsKey("sname1") && matcher.group("sname1") != null
+                               ? matcher.group("sname1") : matcher.group("sname");
+
         log.trace(
-          "Matched donator name with `{}`\n{}\n{}",
+          "Matched donator name with `{}`\n{}\n{}\n{} {}",
           this.getClass().getSimpleName(),
           pattern,
-          value
+          value,
+          fnameGroup,
+          snameGroup
         );
-        return Optional.of(
-          StringUtils.capitalize(matcher.group("sname").toLowerCase(Locale.getDefault()))
-            + " "
-            + StringUtils.capitalize(matcher.group("fname").toLowerCase(Locale.getDefault()))
-        );
+
+        final String fname = StringUtils.capitalize(fnameGroup.toLowerCase(Locale.getDefault()));
+        final String sname = StringUtils.capitalize(snameGroup.toLowerCase(Locale.getDefault()));
+
+        return Optional.of(sname + " " + fname);
       }
       return Optional.empty();
     }
@@ -58,20 +66,21 @@ class PrivatTxNameMapper {
   @RequiredArgsConstructor
   static final class DescriptionNameResolver implements PrivatNameResolver {
 
-    private static final Set<Pattern> PATTERNS = Set.of(
-      compile(".*\\sвнески.*, (?<sname>[^\\p{IsLatin},]{3,})\\s(?<fname>[^\\p{IsLatin}]{3,})\\s[^\\p{IsLatin}]+"),
-      compile("благо.* внес.*, (?<sname>[^\\p{IsLatin},]{3,})\\s(?<fname>[^\\p{IsLatin}]{3,})\\s[^\\p{IsLatin}]+"),
+    private static final List<Pattern> PATTERNS = List.of(
+      compile(".лаго.*\\sвнес.*в.д\\s+(?<fname1>\\w{3,})\\s(?<sname1>\\w{3,}),\\s"
+        + "(?<sname>[^\\p{IsLatin},]{3,})\\s(?<fname>[^\\p{IsLatin}]{3,})\\s[^\\p{IsLatin}]+"),
+      compile(".*\\sвнес.*, (?<sname>[^\\p{IsLatin},]{3,})\\s(?<fname>[^\\p{IsLatin}]{3,})\\s[^\\p{IsLatin}]+"),
       compile("(.плата|збір|допомога|пожертва).*, "
         + "(?<sname>[^\\p{IsLatin},]{3,})\\s(?<fname>[^\\p{IsLatin}]{3,})\\s[^\\p{IsLatin}]+"),
       compile("на.*(зсу|бпла|дрон|рації|перемог.).*, "
         + "(?<sname>[^\\p{IsLatin},]{3,})\\s(?<fname>[^\\p{IsLatin}]{3,})\\s[^\\p{IsLatin}]+"),
       compile(".*благодійність, (?<sname>[^\\p{IsLatin}]{3,})\\s(?<fname>[^\\p{IsLatin}]{3,})\\s[^\\p{IsLatin}]+"),
       compile("\\d{4}.*послуги: (?<sname>\\w{3,}),?\\s(?<fname>\\w{3,}).*"),
-      compile("\\d{4}.*інше: від (?<sname>\\w{3,}),?\\s(?<fname>\\w{3,}).*"),
-      compile("\\d{4}.*iнше: вiд (?<sname>\\w{3,}),?\\s(?<fname>\\w{3,}).*"),
-      compile("\\d{4}.*перекази: вiд (?<sname>\\w{3,})\\s(?<fname>\\w{3,}).*"),
-      compile("\\d{4}.*арахування переказу: (вiд )?(?<sname>\\w{3,})\\s(?<fname>\\w{3,}).*"),
-      compile(".арахування переказу: (вiд )?(?<sname>\\w{3,})\\s(?<fname>\\w{3,}).*")
+      compile("\\d{4}.*інше: в.д (?<sname>\\w{3,}),?\\s(?<fname>\\w{3,}).*"),
+      compile("\\d{4}.*iнше: в.д (?<sname>\\w{3,}),?\\s(?<fname>\\w{3,}).*"),
+      compile("\\d{4}.*перекази: в.д (?<sname>\\w{3,})\\s(?<fname>\\w{3,}).*"),
+      compile("\\d{4}.*арахування переказу: (в.д )?(?<sname>\\w{3,})\\s(?<fname>\\w{3,}).*"),
+      compile(".арахування переказу: (в.д )?(?<sname>\\w{3,})\\s(?<fname>\\w{3,}).*")
     );
 
     private final DonatorsReporterProperties donatorsReporterProperties;
